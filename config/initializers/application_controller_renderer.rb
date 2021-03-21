2302537b20a916ace16a265c80058b58f990beed
koki-takahashi-1107 <jubeatknit18@gmail.com>
2021-03-21 11:06:14 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b13df0d719e915b1fc8eb2683bab737ac0a90b1df35d3ce6f28902bf948f508478cb29d623b0d77bf6cce55a20a4b7bfd027df5320af07143b4849f296f2ffe7'
